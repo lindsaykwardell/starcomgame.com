de2c7d1d77dae47e71b11d55384911a7fd2eed97
Lindsay Wardell <three060@gmail.com>
2022-10-07 14:44:12 -0700

    Add generation for tgc images

--- generate-images.sh
+++ generate-images.sh
@@ -2,15 +2,31 @@
 
 rm -rf tmp
 mkdir tmp
+rm -rf tgc
+mkdir tgc
+mkdir tgc/ships
+mkdir tgc/planets
+mkdir tgc/industry
+mkdir tgc/statecraft
+mkdir tgc/science
 
 gsc -dNOPAUSE -sDEVICE=png16m -r256 -sOutputFile=tmp/%01d.png cards.pdf -c quit;
 
 magick mogrify -format webp tmp/*.png
 
-# for i in tmp/*.png; do sips -s format webp -s formatOptions 70 "${i}" --out "${i%png}webp"; done
 node rename-cards.js
 
 magick mogrify -resize 50% tmp/*-thumb.webp
+
+sips --resampleHeightWidth 1125 825 tgc/industry/*.png
+sips --resampleHeightWidth 1125 825 tgc/statecraft/*.png
+sips --resampleHeightWidth 1125 825 tgc/science/*.png
+sips --resampleHeightWidth 1125 825 tgc/ships/*.png
+sips -r 270 tgc/planets/*.png
+sips --resampleHeightWidth 1125 825 tgc/ships/*.png
+
+cp public/back.jpg tgc/back.jpg
+sips --resampleHeightWidth 1125 825 tgc/back.jpg
 
 rm -rf ./public/cards
 mv ./tmp ./public/cards
--- rename-cards.js
+++ rename-cards.js
@@ -1,12 +1,49 @@
 const csv = require("csv/sync");
 const fs = require("fs");
 
-const main = csv.parse(fs.readFileSync("./cardlist-main.csv"), { columns: true });
-const planets = csv.parse(fs.readFileSync("./cardlist-planets.csv"), { columns: true });
+const main = csv.parse(fs.readFileSync("./cardlist-main.csv"), {
+  columns: true,
+});
+const planets = csv.parse(fs.readFileSync("./cardlist-planets.csv"), {
+  columns: true,
+});
 
 for (const card of [...main, ...planets]) {
-  fs.copyFileSync(`./tmp/${card["Card #"]}.webp`, `./tmp/${card.Name.replace(/ /g, "_")}.webp`);
-  fs.copyFileSync(`./tmp/${card["Card #"]}.webp`, `./tmp/${card.Name.replace(/ /g, "_")}-thumb.webp`);
-  fs.rmSync(`./tmp/${card["Card #"]}.webp`)
-  fs.rmSync(`./tmp/${card["Card #"]}.png`)
+  fs.copyFileSync(
+    `./tmp/${card["Card #"]}.webp`,
+    `./tmp/${card.Name.replace(/ /g, "_")}.webp`
+  );
+  fs.copyFileSync(
+    `./tmp/${card["Card #"]}.webp`,
+    `./tmp/${card.Name.replace(/ /g, "_")}-thumb.webp`
+  );
+  let subfolder;
+  if (card["Card Types"].includes("System")) {
+    subfolder = "planets";
+  } else if (
+    (card["Card Types"].includes("Ship") ||
+      card["Card Types"].includes("Fighter") ||
+      card["Card Types"].includes("Station")) &&
+    card["Domain"] === "Neutral"
+  ) {
+    subfolder = "ships";
+  } else {
+    switch (card["Domain"]) {
+      case "Industry":
+        subfolder = "industry";
+        break;
+      case "Statecraft":
+        subfolder = "statecraft";
+        break;
+      case "Science":
+        subfolder = "science";
+        break;
+    }
+  }
+  fs.copyFileSync(
+    `./tmp/${card["Card #"]}.png`,
+    `./tgc/${subfolder}/${card.Name}[face,${card.Count}].png`
+  );
+  fs.rmSync(`./tmp/${card["Card #"]}.webp`);
+  fs.rmSync(`./tmp/${card["Card #"]}.png`);
 }
