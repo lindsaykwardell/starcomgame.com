59281613b87c789d29da643fb438621d6684f7de
Lindsay Wardell <three060@gmail.com>
2022-10-07 23:32:54 -0700

    Rules update

--- generate-images.sh
+++ generate-images.sh
@@ -10,6 +10,7 @@ mkdir tgc/industry
 mkdir tgc/statecraft
 mkdir tgc/science
 mkdir tgc/back
+mkdir tgc/rules
 
 gsc -dNOPAUSE -sDEVICE=png16m -r256 -sOutputFile=tmp/%01d.png cards.pdf -c quit;
 
@@ -32,6 +33,8 @@ cp public/planet.png tgc/back/planet.png
 cp public/science.png tgc/back/science.png
 cp public/ship.png tgc/back/ship.png
 cp public/statecraft.png tgc/back/statecraft.png
+cp public/frontcover.png tgc/rules/0.png
+cp public/backcover.png tgc/rules/z.png
 
 sips --resampleHeightWidth 1125 825 tgc/back/*.png
 
@@ -39,3 +42,11 @@ rm -rf ./public/cards
 mv ./tmp ./public/cards
 cp cardlist-main.csv ./public/cards/cardlist-main.csv
 cp cardlist-planets.csv ./public/cards/cardlist-planets.csv
+
+npm run rules:pdf
+
+gsc -dNOPAUSE -sDEVICE=png16m -r256 -sOutputFile=tgc/rules/page%02d.png rules/.vuepress/public/star_commander_rulebook.pdf -c quit;
+
+sips --resampleHeightWidth 2475 1575 tgc/rules/*.png
+
+magick convert tgc/rules/*png rules/.vuepress/public/star_commander_rulebook.pdf
