7894730dd8d49bc59b7ba27f66709a52ab94afb9
Lindsay Wardell <three060@gmail.com>
2022-10-29 20:59:54 -0700

    Update metadata

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,51 @@
-import { defineApp } from 'iles'
+import { defineApp } from "iles";
 
-export default defineApp({})
+export default defineApp({
+  head(ctx) {
+    const { frontmatter, site } = ctx;
+    return {
+      meta: [
+        {
+          property: "author",
+          content: frontmatter?.author || "Lindsay Wardell",
+        },
+        {
+          property: "og:site_name",
+          content: "Star Commander",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: frontmatter?.title || site.title,
+        },
+        {
+          property: "og:description",
+          content: frontmatter?.snippet || site.description,
+        },
+        {
+          property: "og:image",
+          content: frontmatter?.image || "/battleship.jpg",
+        },
+        {
+          property: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          property: "twitter:title",
+          content: frontmatter?.title || site.title,
+        },
+        {
+          property: "twitter:description",
+          content: frontmatter?.snippet || site.description,
+        },
+        {
+          property: "twitter:image",
+          content: frontmatter?.image || "/battleship.jpg",
+        },
+      ],
+    };
+  },
+});
